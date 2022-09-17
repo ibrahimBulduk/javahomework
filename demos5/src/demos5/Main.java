0933b211dc5beff7c95840f5aca414dbbde5d8d0
Xlife <Xlife@DESKTOP-33DA86U.Dlink>
2022-09-17 18:40:13 +0300

    sartBlokları

--- demos5/src/demos5/Main.java
+++ demos5/src/demos5/Main.java
@@ -3,7 +3,15 @@ package demos5;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		int sayi = 11 ;
+		if (sayi<10) {
+			System.out.println("sayi 10 dan küçüktür");
+		}else if (sayi==10) {
+			System.out.println("sayı10 eşittir.");
+		}else {
+			System.out.println("sayı 10dan büyüktür");
+		}
+			
 	}
 
 }
